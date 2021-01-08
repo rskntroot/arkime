2e651f2241cffdf15a41be46578458f319e341b3
Lost Monsoon <rskntroot@gmail.com>
2021-01-08 09:30:45 +0000

    testing import with path -v /:/import/

--- scripts/import.sh
+++ scripts/import.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-cd $ARKIME_DIR && ./bin/node viewer.js -c ./etc/config.ini >> ./logs/viewer.log 2>&1
+if [ ! -e /import/ ]; then echo "No PCAP files were found in the directory: /import/";
+else 
+
+# eat pcap for breakfast
+for $PCAP_FILE in $(ls /import/* | grep *.pcap); do
+	$ARKIME_DIR/bin/moloch-capture -r $PCAP_FILE;
+	rm -f $PCAP_FILE;
+done;
+fi
 #'lost'21jn
